d389065d18b4b6cb79bb5dd27a48b06b67793395
Abzal3239 <128156561+Abzal3239@users.noreply.github.com>
2025-12-06 17:34:40 +0530

    Update greeting message for Abzal

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Airah Khan");
-        System.out.println("Hey Abzal, Assu Khan S");
+        System.out.println("Hey Abzal Khan S");
     }
 }
